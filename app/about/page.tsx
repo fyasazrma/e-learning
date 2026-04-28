6d6a30726e79d006fd8580223f9be2bde2f7b8d7
Fayasqii Azhar Mauludin <fyasazrma@gmail.com>
2026-04-28 18:24:01 +0700

    penambahan code biasa

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>About</h1>
+      <p>
+        Aplikasi E-Learning Pelatihan Software Perkantoran Berbasis AI Adaptive
+        Learning.
+      </p>
+    </main>
+  );
+}
